5bcc699150861927cbf486417f66d79f9df9fb14
Runner <821953655@qq.com>
2016-11-20 16:42:44 +0800

    leetcode

---
+++ likanglin/src/com/hust/leetcode/SumUtil.java
@@ -0,0 +1,21 @@
+package com.hust.leetcode;
+
+public class SumUtil {
+	public int[] twoSum(int[] nums, int target) {
+        int result[] =new int[2];
+        for(int i=0;i<nums.length;i++)
+        {
+            for(int j=1;j<nums.length;j++)
+            {
+                if((nums[i]+nums[j])==target && (i!=j))
+                {
+                    result[0]=i;
+                    result[1]=j;
+                    return result;
+                }
+            }
+        }
+        return null;
+        
+    }
+}
