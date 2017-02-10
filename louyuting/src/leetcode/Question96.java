2193f48d638fa416225d11861000253bcf4b3474
louyuting <1849491904@qq.com>
2017-02-10 14:04:57 +0800

    leetcode-louyuting

---
+++ louyuting/src/leetcode/Question96.java
@@ -0,0 +1,26 @@
+package leetcode;
+
+/**
+ * Created by louyuting on 17/2/10.
+ *
+ */
+public class Question96 {
+
+    public static int numTrees(int n) {
+        int [] f = new int[n+1];//利用f[n]来存储最后的结果.
+        f[0] = f[1] = 1;
+
+        for(int i=2; i<=n; ++i) {
+            for(int j=1; j<=i; ++j) {
+                f[i] += f[j-1] * f[i-j];
+            }
+        }
+        return f[n];
+    }
+
+
+    public static void main(String[] args) {
+        //
+        System.out.println(Question96.numTrees(3));
+    }
+}
