81f4dd052f27ba9d2310e0e14f61415023cae702
louyuting <1849491904@qq.com>
2017-05-08 23:58:50 +0800

    leetcode-louyuting

---
+++ minjun/src/com/minjun/Minjun.java
@@ -0,0 +1,11 @@
+package com.minjun;
+
+/**
+ * Created by louyuting on 2017/5/8.
+ */
+public class Minjun {
+
+    public static void main(String[] args) {
+        System.out.println("hello ，I am minjun");
+    }
+}
