2d72d3fb2c8d63aa2fc9bb9ba39db48cb45629fa
louyuting <louyuting@baidu.com>
2017-05-09 09:51:31 +0800

    leetcode-louyuting

--- louyuting/src/leetcode/Question508.java
+++ louyuting/src/leetcode/Question508.java
@@ -1,9 +1,11 @@
 package leetcode;
 
-import leetcode.common.TreeNode;
-
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
+
+import leetcode.common.TreeNode;
 
 /**
  * Created by louyuting on 2017/5/9.
@@ -12,17 +14,19 @@ public class Question508 {
 
     public static Map<Integer, Integer> map= new HashMap<>();
 
-    public static void func(TreeNode root){
-
+    public static int func(TreeNode root){
         if(root == null)
-            return;;
+            return 0;;
 
-        func(root.left);
+        int left  = func(root.left);
+        int right = func(root.right);
 
-        func(root.right);
+        int sum = left+right+root.val;
+        map.put(sum, map.get(sum)==null? 1: map.get(sum)+1);
+        return sum;
     }
 
-    public int[] findFrequentTreeSum(TreeNode root) {
+    public static int[] findFrequentTreeSum(TreeNode root) {
 
         if(root == null)
             return new int[1];
@@ -32,9 +36,33 @@ public class Question508 {
             return res;
         }
 
+        func(root);
 
+        int max=0;
+        for(int key : map.keySet()){
+            max = map.get(key)>max? map.get(key):max;
+        }
 
+        List<Integer> reslist = new ArrayList<Integer>();
+        for(int key : map.keySet()){
+            if(map.get(key) == max){
+                reslist.add(key);
+            }
+        }
+        int[] res = new int[reslist.size()];
+        for (int i=0; i<res.length; i++){
+            res[i] = reslist.get(i);
+        }
+        return res;
+    }
 
-        return null;
+    public static void main(String[] args) {
+        TreeNode root = new TreeNode(5);
+        root.left = new TreeNode(2);
+        root.right = new TreeNode(-5);
+
+        for(int i: findFrequentTreeSum(root)){
+            System.out.println(i);
+        }
     }
 }
